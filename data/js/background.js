10fa6b614775099944702e4a93a67f80c750e95b
Dennis <dennis.aldana@gmail.com>
2014-03-02 12:47:02 -0600

    Initial commit

---
+++ data/js/background.js
@@ -0,0 +1,5 @@
+(function() {
+  // For methods present in all web pages
+  // USEFULL AS A GOOGLE CHROME EXTENTION ONLY
+  // Example: right click share this image
+}).call(this);
---
+++ data/js/tumbleweed.js
@@ -0,0 +1,26 @@
+var layout_opts = {
+        closable:                   true
+    ,   resizable:                  true
+    ,   slidable:                   true
+    ,   enableCursorHotkey:         false
+    ,   paneClass:                  'ui-layout-pane'
+    ,   north__slidable:            false
+    ,   north__closable:            true
+    ,   north__size:                70
+    ,   north__showOverflowOnHover: true
+    ,   north__spacing_open :       0
+    ,   north__spacing_closed:      0
+    ,   north__initClosed:          true
+    ,   south__size:                0
+    ,   south__spacing_open:        0
+    ,   south__spacing_closed:      0
+    ,   south__initClosed:          true
+    ,   south__showOverflowOnHover: true
+    ,   north__paneSelector:        '#header'
+    ,   center__paneSelector:       '#center'
+    ,   south__paneSelector:        '#bottom'
+};
+
+// For default configs and global methods
+// (Depends on the javascript framework we choose)
+
